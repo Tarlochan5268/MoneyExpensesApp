e1105b0b659ca8b50bcfb8ea04fa6cc184e19192
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2021-02-13 23:44:15 -0500

    Initial commit

---
+++ android/app/src/main/java/com/tarlochan/money_expenses_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.tarlochan.money_expenses_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Expense App",
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Expense App")),
+      ),
+      body: Center(child: Text("Hi There")),
+    );
+  }
+}
