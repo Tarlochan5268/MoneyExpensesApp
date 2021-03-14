ea56e736919c02064933b3c6b9401d26f8e5e216
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2021-03-14 15:15:30 -0400

    Optimised and Modified the size and width of chart bars according to screen sizes

--- lib/widgets/chartbars.dart
+++ lib/widgets/chartbars.dart
@@ -9,7 +9,61 @@ class ChartBars extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return LayoutBuilder(builder: (context, constraints) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(
+                  child: Text('\$${spendingAmount.toStringAsFixed(0)}'))),
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
+          ),
+          Container(
+            height: constraints.maxHeight * 0.6,
+            width: 10,
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1,
+                    ),
+                    color: Color.fromRGBO(220, 220, 220, 1).withOpacity(0.7),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                FractionallySizedBox(
+                  heightFactor: spendingPercentageAmount,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.red.withOpacity(0.8),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
+          ),
+          Container(
+            height: constraints.maxHeight * 0.15,
+            child: FittedBox(
+              child: Text(
+                day,
+                style: TextStyle(fontWeight: FontWeight.w900),
+              ),
+            ),
+          ),
+        ],
+      );
+    });
+    /*return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Container(
             height: 15,
@@ -53,6 +107,6 @@ class ChartBars extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.w900),
         ),
       ],
-    );
+    );*/
   }
 }
